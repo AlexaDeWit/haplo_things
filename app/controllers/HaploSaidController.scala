9c0e83226f9d5ff699c6c2b3aeadf5b6c309351d
Alexa DeWit <alexa.dewit@gmail.com>
2014-11-06 12:27:06 -0500

    Resolving my lack of understanding to a compilable state.

--- app/controllers/HaploSaidController.scala
+++ app/controllers/HaploSaidController.scala
@@ -3,19 +3,25 @@ package controllers
 import models._
 import play.api._
 import play.api.mvc._
+import play.api.db.slick._
 import slick.driver.PostgresDriver.simple._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.mvc._
 import play.api.Play.current
 import play.api.mvc.BodyParsers._
+import play.api.libs.json.Json
+import play.api.libs.json.Json._
 
 object HaploSaidController extends Controller {
 
   //query interface for things haplo says.
   val haplo_said = TableQuery[HaploSaid]
 
-  def index = Action {  implicit rs =>
+  def index = DBAction {  implicit rs =>
     val shit_haplo_said = haplo_said.sortBy( _.created_at.desc ).take( 15 ).list
     //replace this with pagination
-    Ok( views.html.index( shit_haplo_said ) )
+    Ok( views.html.haplo_said.index( shit_haplo_said ) )
   }
 
 }
