587ee8cdab431f4bc072ff41cf96f06717fa3485
Alexa DeWit <alexa.dewit@gmail.com>
2014-11-08 00:30:55 -0500

    Working on getting XSS protection working.

--- app/controllers/HaploSaidController.scala
+++ app/controllers/HaploSaidController.scala
@@ -4,11 +4,11 @@ import models._
 import views._
 import play.api._
 import play.api.mvc._
+import play.filters.headers.SecurityHeadersFilter
 import play.api.db.slick._
 import slick.driver.PostgresDriver.simple._
 import play.api.data._
 import play.api.data.Forms._
-import play.api.mvc._
 import play.api.Play.current
 import play.api.mvc.BodyParsers._
 import play.api.libs.json.Json
